2d651016db2ccfb1a740362747630fef8da262b0
Hannah Hall <hannahkhall13@gmail.com>
2021-05-10 13:55:36 -0500

    put trailing slash back

--- levelup/urls.py
+++ levelup/urls.py
@@ -21,7 +21,7 @@ from levelupapi.views import register_user, login_user
 from rest_framework import routers
 from levelupapi.views import GameTypeView
 
-router = routers.DefaultRouter()
+router = routers.DefaultRouter(trailing_slash=False)
 # /gametypes?label=blue
 router.register(r'gametypes', GameTypeView, 'gametype')
 router.register(r'games', GameView, 'game')
